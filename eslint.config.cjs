081004b144c1efe4482f42abe0e3ea9da2224e3f
ShinobuTakahashi <a.little.more.than.before@gmail.com>
2025-01-23 18:42:01 +0900

    Update akashic/eslint-config

---
+++ eslint.config.cjs
@@ -0,0 +1,22 @@
+const eslintConfig = require("@akashic/eslint-config");
+
+module.exports = [
+    ...eslintConfig,
+    {
+        files: ["src/**/*.ts"],
+        languageOptions: {
+            sourceType: "module",
+            parserOptions: {
+                project: "tsconfig.eslint.json",
+            },
+        },
+        ignores: [
+          "*.js",
+          "*.cjs",
+          "*.mjs",
+          "dist/**/*",
+          "lib/**/*",
+          "**/*.d.ts"
+        ]
+    }
+];
